25c6236033c24aaa4a9e25a3f52e0ee7da10339a
佐々木　莉奈 <1072sasaki@gmail.com>
2021-09-05 02:10:30 +0900

    [fix] all

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+
+  #indexメソッド
+  def index
+    @tasks = Task.all
+    #全ての配列を取得
+    @task = Task.new
+    #モデルクラスのインスタンス化(form withでモデルのインスタンスを渡すため)
+  end
+
+  #createメソッド
+  def create
+    task = Task.new(task_params)
+    #モデルクラスのインスタンス化（値を取る）
+    task.save
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+  end
+  # deleteアクションを追加
+  # Task.find(task_params)をtaskに入れて値を取り出す。
+  # task.destroyで消す作業を記述
+
+
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title,:content)
+  end
+end
--- db/migrate/20210904145954_create_tasks.rb
+++ db/migrate/20210904145954_create_tasks.rb
@@ -1,6 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.text :title
+      t.text :content
 
       t.timestamps
     end
