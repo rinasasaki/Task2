51228b36bec9c89880d271c6d0b93716f2ed0809
佐々木　莉奈 <1072sasaki@gmail.com>
2021-09-13 01:02:29 +0900

    [add] edit.html.erb showアクションを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,7 @@ class TasksController < ApplicationController
     task = Task.new(task_params)
     #モデルクラスのインスタンス化（値を取る）
     task.save
+    redirect_to tasks_path
   end
 
   def destroy
@@ -27,12 +28,23 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 #editアクションを追加
-# Task.find(task_params)をtaskに入れて値を取り出す。
+#Task.find(task_params)をtaskに入れて値を取り出す。
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def show
+    task = Task.find(params[:id])
+  end
 
 
   private
 
   def task_params
-    params.require(:task).permit(:title,:content)
+    params.require(:task).permit(:title, :content)
   end
 end
+
