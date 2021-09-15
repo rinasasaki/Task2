dce3ce978fe7e0e6f980de5d8fdf238ab55e87e7
佐々木　莉奈 <1072sasaki@gmail.com>
2021-09-15 14:25:03 +0900

    [fix] tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,42 @@
 class TasksController < ApplicationController
 
-  #indexメソッド
   def index
     @tasks = Task.all
-    #全ての配列を取得
     @task = Task.new
-    #モデルクラスのインスタンス化(form withでモデルのインスタンスを渡すため)
   end
 
-  #createメソッド
+    #indexメソッド
+    #全ての配列を取得
+    #モデルクラスのインスタンス化(form withでモデルのインスタンスを渡すため)
+
   def create
     task = Task.new(task_params)
-    #モデルクラスのインスタンス化（値を取る）
     task.save
     redirect_to tasks_path
   end
+
+    #createメソッド
+    #モデルクラスのインスタンス化（値を取る）
+    #saveメソッドでデータベースに保存
+    #一覧画面に戻る
 
   def destroy
     task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_path
   end
-  # destroyアクションを追加
-  # Task.find(task_params)をtaskに入れて値を取り出す。
-  # task.destroyで消す作業を記述
-  #redirect_to で削除を実行したら一覧画面に戻る。
+
+    # destroyアクションを追加
+    # Task.find(task_params)をtaskに入れて値を取り出す。
+    # task.destroyで消す作業を記述
+    #redirect_to で削除を実行したら一覧画面に戻る。
 
   def edit
     @task = Task.find(params[:id])
   end
-#editアクションを追加
-#Task.find(task_params)をtaskに入れて値を取り出す。
+
+    #editアクションを追加
+    #Task.find(task_params)をtaskに入れて値を取り出す。
 
   def update
     task = Task.find(params[:id])
@@ -38,13 +44,18 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+    #updateメソッド
+    #Task.find(task_params)をtaskに入れて値を取り出す。
+    #更新する
+    #一覧画面に戻る
+
   def show
     @task = Task.find(params[:id])
+  end
 
     # @tasks = Task.all
     # #全ての配列を取得
     # @task = Task.new
-  end
 
 
   private
@@ -52,5 +63,10 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :content)
   end
+
+  #ストロングパラメータでtitleとcontentだけを取得できるようにする
+
 end
+
+    
 
